eaaf400babeb554a61bbab33380f7e545edca967
Erin Mettille <ecmettille@gmail.com>
2020-04-29 19:46:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,11 @@
 class Transfer
-  attr_accessor :status
+  attr_accessor :status, :sender, :receiver, :transfer_amount
   
-  def initialize (sender, receiver)
+  def initialize (sender, receiver, amount)
     @sender = sender 
     @receiver = receiver 
     @status = "pending"
+    @transfer_amount = amount
   end
   
 end
